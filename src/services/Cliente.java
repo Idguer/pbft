772e97261ca93f8abfdb4f2a50019a17f173285f
Idguer <id.guerrero@usal.es>
2026-04-27 19:02:45 +0200

    Ahora muestra un Hola basico

--- src/services/Cliente.java
+++ src/services/Cliente.java
@@ -1,10 +1,20 @@
 package services;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+@Path("Cliente")
 public class Cliente {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	
+	@Path("hola")
+	@GET
+	@Consumes(MediaType.TEXT_PLAIN)
+	public String hola() {
+		return ("¡Hola mundo!");
 	}
 
 }
