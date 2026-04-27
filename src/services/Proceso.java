17ac0ba2738aacfede8a26812c9a559ea6f1ce36
Idguer <id.guerrero@usal.es>
2026-04-27 19:13:32 +0200

    Esqueleto metodos basicos de Proceso

--- src/services/Proceso.java
+++ src/services/Proceso.java
@@ -1,5 +1,17 @@
 package services;
 
 public class Proceso {
+	
+	public void propuesta() {
+		//ToDo();
+	}
+	
+	public void compromiso() {
+		//ToDo();
+	}
+	
+	public void comision() {
+		//ToDo();
+	}
 
 }
