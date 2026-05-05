5cc3bbe5bbb796d9aad6fb31a81a1db7b5d5932c
NachHub <asminach03@gmail.com>
2026-05-05 08:17:12 +0200

    Esqueleto General del Cliente hecho, lo demás depende de la lógica de comunicación que se va a usar

--- src/services/Cliente.java
+++ src/services/Cliente.java
@@ -6,19 +6,69 @@ class Cliente {
 	public final int numProcesos = 6;
 	
 	public static void main(String[] args) {
-		// Instrucciones al usuario
-		System.out.println("Estas son las instrucciones:\n"
-				+ "Fallo (fN): Modifica el estado de fallo del proceso N\n"
-				+ "Cambiar valor (sX): proponer un cambio de valor de la variable a X\n"
-				+ "Estado (s): muestra una tabla separada por tabuladores indicando, para cada nodo, su id, el valor de su variable, los compromisos recibidos y su estado de fallo\n"
-				+ "Ayuda (h): muestra las opciones disponibles y sus combinaciones de teclas.\n");
+		Cliente cliente = new Cliente();
 		
 		String respuesta;
 		Scanner sc = new Scanner(System.in);
-		respuesta = sc.nextLine();
-		sc.close();
 		
-		if(respuesta.length() >= 2) {
+		cliente.ayuda();
+		
+		while (true) {
+			try {
+				System.out.print("> ");
+				respuesta = sc.nextLine().trim().toLowerCase(); // trim permitecoger toda la línea escrita por el
+																// usuario
+
+				if (respuesta.isEmpty()) {
+					System.out.println("Error");
+					cliente.ayuda();
+					continue;
+				}
+
+				char instruccion = respuesta.charAt(0); // coge el caracter en posición 0
+
+				switch (instruccion) {
+					case 'f':
+						if (respuesta.length() > 1) {
+							int idProceso = Integer.parseInt(respuesta.substring(1).trim()); // permitir más de 6
+																								// procesos o no?
+							if (idProceso < 0 || idProceso > numProcesos - 1) {
+								System.out.println("Error: rango de ids 0-5");
+							} else {
+								cliente.fallo(idProceso);
+							}
+						} else {
+							System.out.println("Error: uso fN");
+						}
+						break;
+					case 's':
+						if (respuesta.length() == 1)
+							cliente.mostrarEstado();
+						else {
+							int valor = Integer.parseInt(respuesta.substring(1).trim());
+							if (valor < 0 || valor > 100) {
+								System.out.println("Error: el valor debe estar entre 0 y 100");
+							} else {
+								cliente.cambiarValor(valor);
+							}
+						}
+						break;
+					case 'h':
+						cliente.ayuda();
+						break;
+					default:
+						System.out.println("Error en el formato del comando. Revisa la ayuda (h).");
+				}
+
+			} catch (Exception e) {
+				System.out.println("Error en el formato");
+			}
+		}
+		
+		//sc.close(); solo se puede poner si hay en condición de salida sino da error
+		
+		
+		/*if(respuesta.length() >= 2) {
 			if(respuesta.startsWith("f")) System.out.println("F");
 			else if(respuesta.startsWith("s")) System.out.println("S");
 			else System.out.println(respuesta);
@@ -26,7 +76,7 @@ class Cliente {
 			if(respuesta.startsWith("s")) System.out.println("s");
 			else if(respuesta.startsWith("h")) System.out.println("h");
 			else System.out.println(respuesta);
-		}
+		}*/
 
 		
 		// Hacer Start a los procesos
@@ -37,12 +87,39 @@ class Cliente {
 	
 	
 	// Crear metodo para modificar el estado de fallo de un proceso
+	public void fallo(int id) 
+	{
+		
+	}
 
-	// Crear metodo para cambiar el valor de la variable de un proceso
+	// Crear metodo para cambiar el valor de la variable
+	public void cambiarValor(int x) 
+	{
+		// mandar de alguna forma a los procesos el valor de x
+		for(int i = 0; i < numProcesos; i++) 
+		{
+			
+		}
+	}
 	
 	// Crear metodo para mostrar una tabla con la informacion
-	
+	public void mostrarEstado() 
+	{
+		// cabecera
+		System.out.println("id\tvar\tcompromisos\terror");
+		//el cliente tiene que recibir la información de los procesos
+	}
+		
 	// Crear metodo para mostrar las instrucciones
+	public void ayuda() 
+	{
+		// Instrucciones al usuario
+				System.out.println("Estas son las instrucciones:\n"
+						+ "Fallo (fN): Modifica el estado de fallo del proceso N\n"
+						+ "Cambiar valor (sX): proponer un cambio de valor de la variable a X\n"
+						+ "Estado (s): muestra una tabla separada por tabuladores indicando, para cada nodo, su id, el valor de su variable, los compromisos recibidos y su estado de fallo\n"
+						+ "Ayuda (h): muestra las opciones disponibles y sus combinaciones de teclas.\n");
+	}
 	
 	
 	
